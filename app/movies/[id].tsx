d9f46e213a11db78485dabc9da1fe635d6558845
aleks.valushko <aleks.valushko@yandex.ru>
2025-04-16 16:10:18 +0300

    create reactive-native mobile-movie-app

---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, View, Text} from "react-native";
+import React from "react";
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <Text>MovieDetails</Text>
+        </View>
+    );
+}
+
+export default MovieDetails;
+const styles = StyleSheet.create({});
